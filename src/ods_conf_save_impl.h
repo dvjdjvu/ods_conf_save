200022f68b52216becc4aa623bc8ad91cad457ce
Jessy James <djvu@inbox.ru>
2014-07-21 18:29:59 +0400

    optimize

--- src/ods_conf_save_impl.h
+++ src/ods_conf_save_impl.h
@@ -25,34 +25,12 @@ public:
     ~OdsConfSaveImpl();
     bool getResponce(const HttpRequest& _req, HttpResponce* _res);
     
-    bool addRecord(QString task, QString value, QString key);
-    bool delTask(QString task);
-    bool delTaskKey(QString task, QString key);
-    bool delAll();
-    
-    bool updateTaskKey(QString task, QString value, QString key);
-    
-    QStringList getAll();
-    QStringList getTask(QString task);
-    QString getTaskKey(QString task, QString key);
-    QStringList getTaskList();
-    
-    
     QString getURIFunct(QString uri);
     QString getURIParam(const HttpRequest& _req, QString par);
 
 private:
     ODS::OdsInterface* odsIface; 
     ODS::IObjectManager ioMgr;
-    
-    QMap<QString, QString> conf;
-
-    QJson::Parser parser;
-    QJson::Serializer serializer;
-
-    QVariantMap query;
-    QVariantMap response;
-    QVariantMap error;
     
     OdsConf Conf;
 };
