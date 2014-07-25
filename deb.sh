96e40f36458cfed0367b551d93b0ec4f487653d5
Jessy James <djvu@inbox.ru>
2014-07-25 12:27:00 +0400

    deb package

--- deb.sh
+++ deb.sh
@@ -1,10 +1,3 @@
-PACKAGE="Package: libOdsConfSave"
-VERSION="Version: 1.0"
-SECTION="Section: libs"
-Architecture="Architecture: amd64"
-Maintainer="Maintainer: Jessy James <djvu@inbox.ru>"
-Description="Description: Модуль сохранения настроек приложений"
-
 DEB=libOdsConfSave
 DEBIAN=$DEB/DEBIAN
 
@@ -17,14 +10,6 @@ cp -r lib/libods_conf_save.so $DEB/usr/odsAppSrv/plugins
 
 mkdir -p $DEBIAN/
 touch $DEBIAN/control
-
-echo $PACKAGE > $DEBIAN/control
-echo $VERSION >> $DEBIAN/control
-echo $SECTION >> $DEBIAN/control
-echo $Architecture >> $DEBIAN/control
-echo $Maintainer >> $DEBIAN/control
-echo $Description >> $DEBIAN/control
-
 
 dpkg-deb --build $DEB
 
